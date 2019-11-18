917c05d81f99d957ff23a2e8211af28ddc9ed2df
nipfty <kingstimeent@gmail.com>
2019-11-18 18:04:45 +0100

    JWT Token Integration

---
+++ ARMS/ViewModel/LoginResponseVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace ARMS.ViewModel
+{
+   
+    
+        public class LoginResponseVM
+        {
+            public LoginResponseVM()
+            {
+
+                this.Token = "";
+                this.responseMsg = new HttpResponseMessage() { StatusCode = System.Net.HttpStatusCode.Unauthorized };
+            }
+
+            public string Token { get; set; }
+            public HttpResponseMessage responseMsg { get; set; }
+        }
+    
+}
