74e63f7ab3043b0c0cc178d68e2a5dedb7508953
mariu <mt@timbed.com>
2019-12-10 11:01:48 +0100

    Added new viewmodel for more efficient api communication.

---
+++ ARMS/ViewModel/ApiCallbackMessage.cs
@@ -0,0 +1,14 @@
+﻿namespace ARMS.ViewModel
+{
+    public class ApiCallbackMessage
+    {
+        public string Body;
+        public bool Success;
+
+        public ApiCallbackMessage()
+        {
+        }
+        
+        
+    }
+}
