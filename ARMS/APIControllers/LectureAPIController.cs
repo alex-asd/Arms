3805ab0fa685b22da78b8c56405ddd8a1fbd8df5
mariu <mt@timbed.com>
2019-12-13 16:18:27 +0100

    Added functionality to API Controllers, fiexd bugs.

--- ARMS/APIControllers/LectureAPIController.cs
+++ ARMS/APIControllers/LectureAPIController.cs
@@ -47,6 +47,33 @@ namespace ARMS.APIControllers
             return Ok(new ApiCallbackMessage("Success", true));
         }
 
+        [HttpPost]
+        [Authorize]
+        [Route("reschedule")]
+        public IHttpActionResult RescheduleLecture(int lecture_id, int times)
+        {
+            var lecture = LectureHelper.GetById(lecture_id);
+            for (int i = 0; i < times; i++)
+            {
+                var new_lec = new Lecture(lecture.From.AddDays(7 * (i + 1)), lecture.To.AddDays(7 * (i + 1)),
+                    lecture.CourseID);
+                new_lec.Insert();
+            }
+
+            return Ok(new ApiCallbackMessage("Success", true));
+        }
+
+
+        [HttpPost]
+        [Authorize]
+        [Route("delete")]
+        public IHttpActionResult DeleteLecture(int lecture_id)
+        {
+            var lecture = LectureHelper.GetById(lecture_id);
+            var deleted = lecture.Delete();
+            return Ok(new ApiCallbackMessage(deleted ? "Success" : "Falied", deleted));
+        }
+
         [HttpGet]
         [Authorize]
         [Route("forstudent")]
@@ -78,7 +105,7 @@ namespace ARMS.APIControllers
                 var supervisions = dc.Supervisors.Where(x => x.UserID == curr_user.UserID)
                     .Select(x => x.CourseID).ToList();
                 var upcmoning_lectures = dc.Lectures.Include(x => x.Course)
-                    .Where(x => supervisions.Contains(x.CourseID)  && x.To.CompareTo(DateTime.Now) >= 0)
+                    .Where(x => supervisions.Contains(x.CourseID) && x.To.CompareTo(DateTime.Now) >= 0)
                     .ToList();
                 return Ok(upcmoning_lectures);
             }
